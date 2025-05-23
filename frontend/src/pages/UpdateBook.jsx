43017b23f77eccba2db322fa1981826246a34391
Gaurav singh <143247885+Gaurav-singh101@users.noreply.github.com>
2025-05-23 11:47:07 +0530

    Update UpdateBook.jsx

--- frontend/src/pages/UpdateBook.jsx
+++ frontend/src/pages/UpdateBook.jsx
@@ -40,7 +40,7 @@ const UpdateBook = () => {
                 alert("All fields are required");
             }else{
                 const response = await axios.put(
-                    "http://localhost:1000/api/v1/update-book" ,
+                    "https://booknova-backend.onrender.com/api/v1/update-book" ,
                     Data , 
                     {headers}
                 );
@@ -63,7 +63,7 @@ const UpdateBook = () => {
 useEffect(() => {
       const fetch = async () => {
       const response =  await axios.get(
-          `http://localhost:1000/api/v1/get-book-by-id/${id}`
+          `https://booknova-backend.onrender.com/api/v1/get-book-by-id/${id}`
       );
       setData(response.data.data);
     };
